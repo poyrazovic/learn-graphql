b6dc85ca31f1c19112c7b1a306eca3d265b6a8eb
Sertac Poyraz <sertacpoyrazz@gmail.com>
2018-04-20 16:29:48 +0300

    integration express.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import { graphql, buildSchema } from 'graphql';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+let serverData = {
+    port: '4000',
+    url: {
+        root: '/',
+        graphQL: '/graphql'
+    }
+};
 
 let schema = buildSchema(`
     type Query {
@@ -10,6 +20,20 @@ let root = {
     hello: () => 'Hello World!'
 };
 
-graphql(schema, '{ hello }', root).then(res => {
-    console.log(res);
+let app = express();
+app.get(serverData.url.root, (req, res) => {
+    res.send(`
+        <p>Homepage</p>
+        <a href="${serverData.url.graphQL}">GraphQL</a>
+    `);
+});
+
+app.use(serverData.url.graphQL, graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(serverData.port, () => {
+    console.log(`> Server started! \n> http://localhost:${serverData.port + serverData.url.root}`);
 });
